41aff4e132ce213fff12c1e4f4182e4722478591
humberthomattar <humberthomattar@gmail.com>
2019-02-12 01:08:31 -0200

    inclusao inicial dos arquivos

---
+++ Vagrantfile
@@ -0,0 +1,43 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+# global vars
+api_version = "2"
+vm_name = "coinscraper"
+sync_dir = "/tmp/coinscraper/"
+ansible_dir = "/tmp/coinscraper/ansible/"
+is_proxy = false
+
+Vagrant.configure(api_version) do |config|
+  
+  config.vm.box = "ubuntu/xenial64" # server v16.04.05
+  config.vm.define vm_name
+  config.vm.hostname = vm_name
+  config.vm.box_check_update = false
+
+  # avaliacao da existencia do proxy
+  if Vagrant.has_plugin?("vagrant-proxyconf") && is_proxy
+    config.proxy.http     = "http://prxdf.prevnet:3128/"
+    config.proxy.https    = "http://prxdf.prevnet:3128/"
+  end
+
+  # Mapeamento das portas guest e host
+  config.vm.network "forwarded_port", guest: 3000, host: 3000
+
+  # Mapeamento de sincronismo host e guest  
+  config.vm.synced_folder ".", sync_dir
+  
+  # configuracoes gerais da maquina
+  config.vm.provider "virtualbox" do |vb|
+    # vb.gui = true
+    vb.memory = "3072"
+    vb.cpus = 2
+    vb.name = vm_name
+  end
+ 
+  # configuracao para o provisionamento do conteudo da maquina
+  config.vm.provision "ansible_local" do |ansible|
+    ansible.playbook = "playbook.yml"
+    ansible.provisioning_path = ansible_dir
+  end
+end
---
+++ coinscraper.py.py
@@ -0,0 +1,70 @@
+import time
+import urllib.request
+from pathlib import Path
+
+import pandas as pd
+from bs4 import BeautifulSoup
+
+
+def all_currencies():
+    """Get List of all Currencies
+
+    Returns:
+        [List] -- [List of all Currencies]
+    """
+    coins = []
+    url = 'https://coinmarketcap.com/all/views/all/'
+
+    html_page = urllib.request.urlopen(url)
+    soup = BeautifulSoup(html_page, features="lxml")
+    all_links = [a.get('href') for a in soup.find_all('a', href=True)]
+
+    for link in list(set(all_links)):
+        if '/currencies/' in link:
+            coins.append(link.split('/')[2])
+
+    return coins
+
+def download_data(currency, start_date, end_date):
+    """Download Historical Data to File
+
+    Arguments:
+        currency {[str]} -- [currency of crypto]
+        start_date {[str]} -- [date yyyymmdd]
+        end_date {[str]} -- [date yyyymmdd]
+    """
+
+    print("Running " + currency)
+
+    try:
+        # Get Html to DataFrame
+        url = 'https://coinmarketcap.com/currencies/' + currency + '/historical-data/' + '?start=' \
+            + start_date + '&end=' + end_date
+        df = pd.read_html(url, attrs={'class': 'table'})[0]
+
+        # Clean data
+        df.columns = ['Date', 'Open', 'High',
+                      'Low', 'Close', 'Volume', 'Market Cap']
+        df['Date'] = pd.to_datetime(df['Date'], format='%b %d, %Y')
+
+        # Save to CSV
+        df.to_csv('data/' + currency + '.csv', index=False)
+    except Exception as e:
+        print("Error : Could not Download - " + currency)
+        print(e)
+
+
+if __name__ == "__main__":
+
+    # Download all Historical Data
+    currencies = all_currencies()
+
+    today = pd.to_datetime('today').strftime('%Y%m%d')
+
+    for currency in currencies:
+
+        my_file = Path('data/' + currency + '.csv')
+
+        if my_file.is_file() == False:
+            time.sleep(2) #website now stops downloads faster than this :(
+            download_data(currency, '20000101', today)
